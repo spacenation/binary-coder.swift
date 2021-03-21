a1986e9d5228ef07dd69709ec3e48020f02110a6
Alex <alex.buzov@icloud.com>
2021-03-21 12:54:17 -0700

    functional decoder

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         .library(name: "BinaryDecoder", targets: ["BinaryDecoder"])
     ],
     dependencies: [
-        .package(name: "Decoder", url: "git@github.com:spacenation/decoder.swift.git", from: "0.3.1"),
+        .package(name: "Decoder", url: "git@github.com:spacenation/decoder.swift.git", from: "0.4.0"),
         .package(name: "Binary", url: "git@github.com:spacenation/binary.swift.git", from: "0.1.1")
     ],
     targets: [
