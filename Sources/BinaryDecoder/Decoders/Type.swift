931cc4dad48885e06e26c2e5299899ca32efab3e
Alex <alex.buzov@icloud.com>
2021-04-22 15:35:03 -0700

    type count fix

--- Sources/BinaryDecoder/Decoders/Type.swift
+++ Sources/BinaryDecoder/Decoders/Type.swift
@@ -2,7 +2,7 @@ import Foundation
 import Binary
 
 public func type<T>(_ type: T.Type) -> Decoder<Bit, T> {
-    byte.count(MemoryLayout<T>.size)
+    byte.count(UInt(MemoryLayout<T>.size))
         .map { data in
             withUnsafeBytes(of: Data(data).prefix(MemoryLayout<T>.size / 8)) {
                 $0.baseAddress!.assumingMemoryBound(to: T.self).pointee
