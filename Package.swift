42756be254b97ce4472d30a8d74052cfd75e3376
Alexander Young <alex.buzov@icloud.com>
2021-06-30 23:35:25 -0700

    dependencies update

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         .library(name: "BinaryDecoder", targets: ["BinaryDecoder"])
     ],
     dependencies: [
-        .package(name: "Decoder", url: "git@github.com:spacenation/swift-decoder.git", from: "0.2.3"),
+        .package(name: "Decoder", url: "git@github.com:spacenation/swift-decoder.git", from: "0.2.5"),
         .package(name: "Binary", url: "git@github.com:spacenation/swift-binary.git", from: "0.1.7")
     ],
     targets: [
